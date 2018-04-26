6e9ca2639a6e397aafb564b563ea753f9108c166
DimaObora <obora.dima@yandex.ru>
2018-04-26 23:14:49 +0300

    добавил все

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function ($) {
+    ymaps.ready(init);
+
+    function init() {
+
+        var myMap;
+
+        myMap = new ymaps.Map("map", {
+            center: [45.350958421605284,39.05811229735281],
+            zoom: 14,
+            scroll:true,
+            controls: []
+        });
+
+        myMap.behaviors.disable('scrollZoom');
+
+        myMap.controls.add("zoomControl", {
+            position: {top: 15, left: 15}
+        });
+        var html  = '<div class="popup">';
+        html +=     '<img src="images/housJPG.jpg" alt="" />';
+        html +=     '<div class="popup-text">';
+        html +=         '<p>Старомышастовская <br> улица Садовая № 211 <br> <a class="tel" href="tel:+79885242737">+79885242737</a>&nbsp;&nbsp;&nbsp;<a class="tel" href="tel:+79002475139">+79002475139</a></p>';
+        html +=     '</div>';
+        html += '</div>';
+
+        var myPlacemark = new ymaps.Placemark([45.350958421605284,39.05811229735281], {
+                balloonContent: html
+            },
+            {
+                iconLayout: 'default#image',
+                iconImageHref: 'images/icon.png',
+                iconImageSize: [50, 50],
+                iconImageOffset: [-33, -50],
+                balloonShadow: false });
+
+
+        myMap.geoObjects.add(myPlacemark);
+
+    }
+
+});
