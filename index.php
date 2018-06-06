704a6d8d92ae21eb83351978eb5b6bc2c3d416d0
ikor <igor_korenovsk@mail.ru>
2018-06-07 01:54:54 +0300

    сделал меню и вывод страниц из меню по щаблону

--- index.php
+++ index.php
@@ -7,44 +7,62 @@ get_header();
 ?>
 <body>
 <div id="wrapper">
-<?php
+    <?php
 
     // Хэдер
-    get_template_part('template-header','none');
+    get_template_part('template-header', 'none');
 
     // Главная
-    get_template_part('template-home','none');
+    get_template_part('template-home', 'none');
+
+    //вывод всех страниц из меню
+    //todo: нужно делать проверку что это за страницы и использовать правильные шаблоны в соответствии с типом
+    if (($locations = get_nav_menu_locations()) && isset($locations['primary'])) {
+        $menu = wp_get_nav_menu_object($locations['primary']);
+        $menu_items = wp_get_nav_menu_items($menu->term_id);
+        if ($menu_items = wp_get_nav_menu_items($menu->term_id)) {
+            $menu_list = '';
+            global $post;
+            foreach ((array)$menu_items as $key => $menu_item) {
+                $post = get_post(intval($menu_item->object_id));
+                setup_postdata($post);
+                get_template_part('template-section', 'none');
+            }
+            wp_reset_postdata();
+        }
+    }
+
 
     // О нас
-    get_template_part('template-about','none');
+    get_template_part('template-about', 'none');
 
     // Коротко о нас
-    get_template_part('template-faq-sec','none');
+    get_template_part('template-faq-sec', 'none');
 
     // Сервис
-    get_template_part('template-service','none');
+    get_template_part('template-service', 'none');
 
     // Позвоните нам
-    get_template_part('template-call-us','none');
+    get_template_part('template-call-us', 'none');
 
     // Продвижение сайтов
-    get_template_part('template-features-desc','none');
+    get_template_part('template-features-desc', 'none');
 
     // Наши проекты
-    get_template_part('template-projects','none');
+    get_template_part('template-projects', 'none');
 
     // Клиенты
-    get_template_part('template-clients','none');
+    get_template_part('template-clients', 'none');
 
     // Комментарии
-    get_template_part('template-testimonials','none');
+    get_template_part('template-testimonials', 'none');
 
     // Контакты
-    get_template_part('template-contacts','none');
+    get_template_part('template-contacts', 'none');
 
     // Футер
-    get_template_part('template-footer','none');
-?>
+    get_template_part('template-footer', 'none');
+    ?>
 
 </div>
 <?php
--- template-header.php
+++ template-header.php
@@ -7,37 +7,26 @@
  */
 ?>
 <header class="st-navbar">
-
     <nav class="navbar navbar-expand-lg fixed-top d-flex justify-content-center navbar-dark bg-dark">
         <div class="container">
-
             <a class="navbar-brand " href="#">Web-студия "ORIGINALS"</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav m-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Наши услуги <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Коротко о нас</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link " href="#">Наши проекты</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " href="#">Контакты</a>
-                    </li>
-                </ul>
-                <div class="form-inline my-2 my-lg-0">
-                    <a class="phone " href="tel:+79002475139">+7(900)24-75-139</a>
-                </div>
-            </div>
-
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'primary',
+                'container' => 'div',
+                'container_class' => 'collapse navbar-collapse',
+                'container_id' => 'navbarSupportedContent',
+                'items_wrap' => '<ul id="%1$s" class="%2$s navbar-nav m-auto">%3$s</ul>
+                                    <div class="form-inline my-2 my-lg-0">
+                                    <a class="phone " href="tel:+79002475139">+7(900)24-75-139</a>',
+                'walker' => new Walker_Primary_Menu()
+            ));
+            ?>
         </div>
     </nav>
 
