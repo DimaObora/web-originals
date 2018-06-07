14245409eafee5427efa735f3e7bcbaf8c416842
ikor <igor_korenovsk@mail.ru>
2018-06-07 17:08:57 +0300

    сделал цитаты

--- template-testimonials.php
+++ template-testimonials.php
@@ -1,53 +1,43 @@
-<section class="testimonials">
-    <div class="container">
-        <div class="row">
-            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                <div class="testimonials-carousel">
-                    <ul>
-                        <li>
-                            <div class="testimonial">
-                                <div class="testimonial-img">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/photos/client.jpg" alt="">
-                                </div>
-                                <blockquote>
-                                    <p>Tueri tantis inter variis deterritum facta caret pleniorem, efficiat affert
-                                        quiete, commodis comparat facio ponti, adolescens recta iucundius mundi
-                                        nostrum viris quae utilitatibus.</p>
-                                    <footer>Joseph Thompson, <cite title="Source Title">Example Inc.</cite></footer>
-                                </blockquote>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="testimonial">
-                                <div class="testimonial-img">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/photos/client2.jpg" alt="">
-                                </div>
-                                <blockquote>
-                                    <p>Contrariis labore vetuit scaevola, contra percurri adamare efficeret quibus.
-                                        Nostram consulatu mediocritatem maiorem, cyrenaicisque, quandam accedit
-                                        veniat cognitioque, animadvertat accusantibus temporibus maximeque
-                                        litterae.</p>
-                                    <footer>Nancy Ford, <cite title="Source Title">Example Inc.</cite></footer>
-                                </blockquote>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="testimonial">
-                                <div class="testimonial-img">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/photos/client3.jpg" alt="">
-                                </div>
-                                <blockquote>
-                                    <p>Illas, volumus prosperum. Nostras eoque statua cuius corrumpit praetor aliter
-                                        quaeso propter ei, quam inducitur ruant doctiores sanguinem atomum
-                                        molestiae, antiqua inculta dicent.</p>
-                                    <footer>Arthur Fernandez, <cite title="Source Title">Example Inc.</cite>
-                                    </footer>
-                                </blockquote>
-                            </div>
-                        </li>
-                    </ul>
+<?php $args = array('posts_per_page' => -1, 'post_type' => 'quote', 'post_status' => 'publish');
+$myposts = get_posts($args);
+if (count($myposts) > 0) {
+    ?>
+
+    <section class="testimonials">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                    <div class="testimonials-carousel">
+                        <ul>
+                            <?php
+                            foreach ($myposts as $post) {
+                                setup_postdata($post);
+                                ?>
+                                <li>
+                                    <div class="testimonial">
+                                        <div class="testimonial-img">
+                                            <img src="<?php echo wp_get_attachment_image_url(get_post_meta($post->ID, "image", true), 'full'); ?>"
+                                                 alt="">
+                                        </div>
+                                        <blockquote>
+                                            <p><?php the_content(); ?></p>
+                                            <footer><?php echo get_post_meta($post->ID, "author", true); ?>, <cite
+                                                        title="Source Title"><?php echo get_post_meta($post->ID, "corp", true); ?></cite>
+                                            </footer>
+                                        </blockquote>
+                                    </div>
+                                </li>
+                                <?php
+                            }
+                            ?>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+
+    <?php
+    wp_reset_postdata();
+}
+?>
