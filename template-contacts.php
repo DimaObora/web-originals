64a37634201d260389cdb1c0ed0442daf0308bcc
DimaObora <obora.dima@yandex.ru>
2018-06-10 16:48:33 +0300

    переделал контакты добавил форму обратной связи

--- template-contacts.php
+++ template-contacts.php
@@ -16,29 +16,37 @@
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 align-self-center">
-                <div class="contacts-visible col-xl- col-lg-4 col-md-12 col-sm-12 d-block d-sm-none d-none d-sm-block d-md-none">
-                    <ul class="width-ul-contactss">
-                        <li class="address "><p class="inl-dl">Старомышастовская улица <br> Садовая № 211</p></li>
-                        <li class="phone "><p class="inl-dl"><a class="tel" href="tel:+79002475139">+79002475139</a></p></li>
-                        <li class="phone "><p class="inl-dl"><a class="tel" href="tel:+79885242737">+79885242737</a></p></li>
-                        <li class="email "><p class="inl-dl"> <a class="mail" href="mailto:web-originals@yandex.ru">web-originals@yandex.ru</a></p></li>
-                    </ul>
+        <div class="container">
+            <div class="row">
+                <div class=" col-xl-6 col-lg-6 col-md-12 col-sm-12 ">
+                    <div class="contacts">
+                        <ul class="width-ul-contactss">
+                            <li class="address "><p class="inl-dl">Старомышастовская улица <br> Садовая № 211</p></li>
+                            <li class="phone "><p class="inl-dl"><a class="tel" href="tel:+79002475139">+79002475139</a>
+                                </p></li>
+                            <li class="phone "><p class="inl-dl"><a class="tel" href="tel:+79885242737">+79885242737</a>
+                                </p></li>
+                            <li class="email "><p class="inl-dl m-b-0"><a class="mail"
+                                                                          href="mailto:web-originals@yandex.ru">web-originals@yandex.ru</a>
+                                </p></li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="contacts col-xl-4 col-lg-4 col-md-5 col-sm-4 d-none d-md-block">
-                    <ul class="width-ul-contactss">
-                        <li class="address "><p class="inl-dl">Старомышастовская улица <br> Садовая № 211</p></li>
-                        <li class="phone "><p class="inl-dl"><a class="tel" href="tel:+79002475139">+79002475139</a></p></li>
-                        <li class="phone "><p class="inl-dl"><a class="tel" href="tel:+79885242737">+79885242737</a></p></li>
-                        <li class="email "><p class="inl-dl"> <a class="mail" href="mailto:web-originals@yandex.ru">web-originals@yandex.ru</a></p></li>
-                    </ul>
-                </div>
-                <div id="map"></div>
-                <div class="coll-back">
-
+                <div class=" coll-back col-xl-6 col-lg-6 col-md-12 col-sm-12">
+                    <form>
+                        <input name="name" placeholder="Укажите ваше имя!" class="textbox" required/>
+                        <input name="emailaddress" placeholder="Укажите ваш Email!" class="textbox" type="email"
+                               required/>
+                        <textarea rows="4" cols="50" name="subject" placeholder="Введите ваше сообщение:"
+                                  class="message" required></textarea>
+                        <input name="submit" class="button" type="submit" value="Отправить"/>
+                    </form>
                 </div>
             </div>
         </div>
+
+        <div id="map"></div>
     </div>
+
+
 </section>
