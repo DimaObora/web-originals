3702cd3f2ef529514deae3b847f623ec5fd037dc
DimaObora <obora.dima@yandex.ru>
2018-06-13 16:47:09 +0300

    пофиксил2 выподающее меню с телефонами

--- template-call-us.php
+++ template-call-us.php
@@ -4,11 +4,11 @@
 <section class="call-us">
     <div class="container">
         <div class="row">
-            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12" id="call">
                 <h3>Если вы хотите работать с нами</h3>
                 <ul class="main-menu">
                     <li>
-                        <a href="#" class="btn btn-default-o btn-lg tel">Позвоните нам сейчас</a>
+                        <a href="#call" class="btn btn-default-o btn-lg tel">Позвоните нам сейчас</a>
                         <ul class="sub-menu">
                             <li><a href="tel:+79002475139" class="color-bl">+79002475139</a></li>
                             <li><a href="tel:+79885242737" class="color-bl">+79885242737</a></li>
