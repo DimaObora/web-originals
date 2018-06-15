c35a617894ba87172b37f945b78b4090a859de96
ikor <igor_korenovsk@mail.ru>
2018-06-15 10:03:02 +0300

    изменил подключение шрифтов

--- header.php
+++ header.php
@@ -6,7 +6,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="http://allfont.ru/allfont.css?fonts=broken-planewing" rel="stylesheet" type="text/css"/>
+<!--    <link href="http://allfont.ru/allfont.css?fonts=broken-planewing" rel="stylesheet" type="text/css"/>-->
+    <style>
+        @font-face {
+            font-family: 'Broken Planewing';
+            font-style: normal;
+            font-weight: 400;
+            src: local('Broken Planewing'), local('BrokenPlanewing-Regular'),
+            url(<?php echo get_template_directory_uri(); ?>/fonts/broken-planewing_d5205167beb8a28313f210963efba9c6.woff) format('woff'),
+            url(<?php echo get_template_directory_uri(); ?>/fonts/broken-planewing_d5205167beb8a28313f210963efba9c6.ttf) format('truetype');
+        }
+    </style>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
           integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <link type="text/css" rel="StyleSheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css"/>
